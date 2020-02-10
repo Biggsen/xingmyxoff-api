1fe36845d7b04d26b55b87afd84ed383894b6027
Biggs Thorarensen <biggs@devillion.com>
2020-02-10 10:30:12 +0000

    noun and verb api endpoints. api key in env

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const fetch = require('node-fetch')
+require('dotenv').config()
+const app = express()
+
+app.get('/getnoun', (req, res) => {
+   //build api URL with user zip
+   const baseUrl = 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=1000&maxCorpusCount=10000&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1'
+   //ENTER YOUR API KEY HERE (make sure to no include < >)
+   const apiId = '&api_key=' + process.env.API_KEY
+
+   const apiUrl = baseUrl + apiId
+
+   fetch(apiUrl)
+   .then(res => res.json())
+   .then(data => {
+      res.send({ data })
+   })
+   .catch(err => {
+      res.redirect('/error')
+   })
+})
+
+app.get('/getverb', (req, res) => {
+   //build api URL with user zip
+   const baseUrl = 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=verb&minCorpusCount=1000&maxCorpusCount=10000&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1'
+   //ENTER YOUR API KEY HERE (make sure to no include < >)
+   const apiId = '&api_key=' + process.env.API_KEY
+
+   const apiUrl = baseUrl + apiId
+
+   fetch(apiUrl)
+   .then(res => res.json())
+   .then(data => {
+      res.send({ data })
+   })
+   .catch(err => {
+      res.redirect('/error')
+   })
+})
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000')
+})
